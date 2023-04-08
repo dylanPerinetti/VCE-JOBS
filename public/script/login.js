16a24c48969b93d4bcf9385bc2a6905d793435b1
Dylan Perinetti <101599798+dylanPerinetti@users.noreply.github.com>
2023-04-08 19:13:35 +0200

    Delete public/script directory

--- public/script/login.js
+++
@@ -1,9 +0,0 @@
-// Obtenir la fenêtre login
-var login = document.getElementById('id01');
-
-// Lorsque l'utilisateur clique n'importe où en dehors de la fenêtre login, la fermer.
-window.onclick = function(event) {
-    if (event.target == login) {
-        login.style.display = "none";
-    }
-}
